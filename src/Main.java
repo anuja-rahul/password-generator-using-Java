ad7857b45549d13d2b0e216067eb7e0a0d62d371
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-03-29 23:01:24 +0530

    minor changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+
+import java.util.*;
 public class Main {
+    Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
     }
 }
